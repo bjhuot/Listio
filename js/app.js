7f534f5bc53a9f12ce514012523ae0aed0efea39
Brian Huot <brian.huot@gmail.com>
2019-02-05 19:55:40 -0500

    incl jquery file

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var inputTitle = '';
+var inputDetail = '';
+const btnSave;
+const btnDel;
+const btnUp;
+const btnDown;
+
+//Page starts with one blank list entry pair, and save button (plus checkbox)
+$('ul').append('<li>hello, world</li>');
+//Clicking save:
+    //Changes button to delete
+        //Check if more than saved pair, generate up/down/delete triplet
+        //if no pairs remaining after clicking delete, button must generate new blank pair
+    //generates new blank pair
+
+//Right side of page has text entry field
+//Print button at bottom prints entire list/notepad
