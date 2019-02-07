0a72400bccf846abe781e142a2fb32145339d3e7
Brian Huot <brian.huot@gmail.com>
2019-02-07 15:02:59 -0500

    defined fields and buttons

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
-var inputTitle = '';
-var inputDetail = '';
-const btnSave;
-const btnDel;
-const btnUp;
-const btnDown;
+var inputName = '<input type="text" name="inputName" placeholder="To-Do Item"></input>';
+var inputDesc = '<input type="text" name="inputDesc" placeholder="To-Do Description"></input>';
+const btnSave = '<div class="btnSave"><button class="save" type="button" value="save">Save</button></div>';
+
+// Contains buttons for Up/Down/Delete as a single div/entity:
+const btnTriplet = '<div class="btnTriplet"><button class="up" type="button" value="up">Up</button><button class="Down" type="button" value="down">Down</button><button class="delete" type="button" value="delete">Delete</button></div>';
 
 //Page starts with one blank list entry pair, and save button (plus checkbox)
-$('ul').append('<li>hello, world</li>');
+    $('.list ul').append('<li/>');
+    $('ul li').addClass('inputItem');
+    //add variable for index to reference each item?
+    $('.inputItem').html('<div>'+inputName+'<br>'+inputDesc+'</div>'+btnSave);
+
+    
 //Clicking save:
     //Changes button to delete
         //Check if more than saved pair, generate up/down/delete triplet
