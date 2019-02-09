b9893b7cc5c360c113551275aa11da040f13e63f
Brian Huot <brian.huot@gmail.com>
2019-02-08 21:26:11 -0500

    got save/postBlank working

--- js/app.js
+++ js/app.js
@@ -1,22 +1,53 @@
+//
+// VARIABLES AND CONSTANTS
+//
+
 var inputName = '<input type="text" name="inputName" placeholder="To-Do Item"></input>';
 var inputDesc = '<input type="text" name="inputDesc" placeholder="To-Do Description"></input>';
-const btnSave = '<div class="btnSave"><button class="save" type="button" value="save">Save</button></div>';
+const btnSave = '<button class="btnSave" type="button" value="save">Save</button>';
 
 // Contains buttons for Up/Down/Delete as a single div/entity:
 const btnTriplet = '<div class="btnTriplet"><button class="up" type="button" value="up">Up</button><button class="Down" type="button" value="down">Down</button><button class="delete" type="button" value="delete">Delete</button></div>';
 
 //Page starts with one blank list entry pair, and save button (plus checkbox)
-    $('.list ul').append('<li/>');
-    $('ul li').addClass('inputItem');
     //add variable for index to reference each item?
-    $('.inputItem').html('<div>'+inputName+'<br>'+inputDesc+'</div>'+btnSave);
 
-    
+//
+// FUNCTIONS
+//
+
+function postBlank() {
+    $('.list ul').append('<li/>');
+    $('ul li:last-child').addClass('inputItem');
+    $('.list ul').find('li:last-child').html('<div>'+inputName+'<br>'+inputDesc+'</div>'+'<div>'+btnSave+'</div>');
+};
+
+//
+// WORKSPACE
+//
+
+$(document).ready(function(){
+    postBlank();
+});
+
 //Clicking save:
-    //Changes button to delete
+$('ul').click(function(){
+    if ( event.target.className == 'btnSave') {
+        $('.btnSave').parent().remove();
+        $('.inputItem:last-child').append(btnTriplet);
+        postBlank();
+    }
+});
+    //Changes button to triplet
         //Check if more than saved pair, generate up/down/delete triplet
         //if no pairs remaining after clicking delete, button must generate new blank pair
-    //generates new blank pair
+    //generates new blank pairs
+
+//Clicking Up moves selected list entry up:
+
+//Clicking Down moves selected list entry down:
+
+//Clicking Delete removes selected list entry:
 
 //Right side of page has text entry field
 //Print button at bottom prints entire list/notepad
