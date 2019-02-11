d523da2f71a9b3f162a632ef686f2c73b12f5276
Brian Huot <brian.huot@gmail.com>
2019-02-11 14:36:57 -0500

    begin styling

--- js/app.js
+++ js/app.js
@@ -2,15 +2,12 @@
 // VARIABLES AND CONSTANTS
 //
 
-var inputName = '<input type="text" name="inputName" placeholder="To-Do Item"></input>';
-var inputDesc = '<input type="text" name="inputDesc" placeholder="To-Do Description"></input>';
+const inputName = '<input type="checkbox" name"inputCheck"><input type="textarea" name="inputName" placeholder="To-Do Item"></input>';
+const inputDesc = '<textarea type="text" name="inputDesc" placeholder="To-Do Description"></textarea>';
 const btnSave = '<button class="btnSave" type="button" value="save">Save</button>';
 
 // Contains buttons for Up/Down/Delete as a single div/entity:
-const btnTriplet = '<div class="btnTriplet"><button class="up" type="button" value="up">Up</button><button class="Down" type="button" value="down">Down</button><button class="delete" type="button" value="delete">Delete</button></div>';
-
-//Page starts with one blank list entry pair, and save button (plus checkbox)
-    //add variable for index to reference each item?
+const btnTriplet = '<div class="btnTriplet"><button class="up" type="button" value="up">Up</button><button class="down" type="button" value="down">Down</button><button class="delete" type="button" value="delete">Delete</button></div>';
 
 //
 // FUNCTIONS
@@ -22,10 +19,6 @@ function postBlank() {
     $('.list ul').find('li:last-child').html('<div>'+inputName+'<br>'+inputDesc+'</div>'+'<div>'+btnSave+'</div>');
 };
 
-function deleteLine() {
-    $(this).remove();
-};
-
 //
 // WORKSPACE
 //
@@ -33,8 +26,10 @@ function deleteLine() {
 $(document).ready(function(){
     postBlank();
 });
-
 //Clicking save:
+    //Changes button to triplet
+        //Check if more than saved pair, generate up/down/delete triplet
+    //generates new blank pairs
 $('ul').click(function(){
     if ( event.target.className == 'btnSave') {
         $('.btnSave').parent().remove();
@@ -42,22 +37,52 @@ $('ul').click(function(){
         postBlank();
     }
 });
-    //Changes button to triplet
-        //Check if more than saved pair, generate up/down/delete triplet
-        //if no pairs remaining after clicking delete, button must generate new blank pair
-    //generates new blank pairs
+
 
 //Clicking Up moves selected list entry up:
+$('ul').click(function(){
+    if(event.target.className == 'up') {
+        var upShift = $(event.target).parent().parent();
+        upShift.insertBefore(upShift.prev());
+        //Marks up button disabled if it reaches the top:
+        if(upShift.index() == 0) {
+            $(event.target).attr('disabled','disabled');
+        };
+
+        //Reverses down status:
+        if(event.target.nextElementSibling.getAttribute('disabled') == 'disabled') {
+            $(event.target).next().removeAttr('disabled');
+        };
+    };
+});
 
 //Clicking Down moves selected list entry down:
+$('ul').on('click', function(){
+    if(event.target.className == 'down') {
+        const downShift = $(event.target).parent().parent();
+        downShift.insertAfter(downShift.next());
+        
+        //Reverses up status:
+        if(event.target.previousElementSibling.getAttribute('disabled') == 'disabled') {
+            $(event.target).prev().removeAttr('disabled');
+        };
+
+        //Marks down button disabled if it reaches the bottom: >>>>>><<<<<<
+        $('li:last-child').prev().on('mouseover', function() {
+            if(downShift.next().index() == $('li:last-child').index()) {
+                if($('.down')) {
+                    $(event.target).attr('disabled','disabled');
+                };
+            };
+        });
+    };
+});
 
 //Clicking Delete removes selected list entry:
 
 $('ul').click(function(){
     if(event.target.className == 'delete') {
         $(event.target).parent().parent().remove();
-        //vs
-        //deleteLine();
     }
 });
 
