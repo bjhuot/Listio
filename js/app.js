34d3f539c4fcf23886f86751a569c1027fb7cc54
Brian Huot <brian.huot@gmail.com>
2019-02-12 16:42:37 -0500

    reworking in vanilla js

--- js/app.js
+++ js/app.js
@@ -1,90 +1,57 @@
 //
-// VARIABLES AND CONSTANTS
+// VARIABLES
 //
 
-const inputName = '<input type="checkbox" name"inputCheck"><input type="textarea" name="inputName" placeholder="To-Do Item"></input>';
-const inputDesc = '<textarea type="text" name="inputDesc" placeholder="To-Do Description"></textarea>';
-const btnSave = '<button class="btnSave" type="button" value="save">Save</button>';
+const li = document.createElement('LI');
+const input = document.createElement('INPUT');
+const buttonup = document.createElement('BUTTON');
+const buttondown = document.createElement('BUTTON');
+const buttonremove = document.createElement('BUTTON');
+const textarea = document.createElement('TEXTAREA');
 
-// Contains buttons for Up/Down/Delete as a single div/entity:
-const btnTriplet = '<div class="btnTriplet"><button class="up" type="button" value="up">Up</button><button class="down" type="button" value="down">Down</button><button class="delete" type="button" value="delete">Delete</button></div>';
 
 //
 // FUNCTIONS
 //
 
-function postBlank() {
-    $('.list ul').append('<li/>');
-    $('ul li:last-child').addClass('inputItem');
-    $('.list ul').find('li:last-child').html('<div>'+inputName+'<br>'+inputDesc+'</div>'+'<div>'+btnSave+'</div>');
+function inputPair() {
+    input.classList.add('itemInput');
+    input.setAttribute('placeholder', 'To-Do');
+    textarea.classList.add('itemDesc');
+    textarea.setAttribute('placeholder', 'Details');
+    li.appendChild(input);
+    li.appendChild(textarea);
 };
 
-//
-// WORKSPACE
-//
+function buttonUp() {
+    buttonup.classList.add('btnUp');
+    buttonup.textContent = 'Up';
+    li.appendChild(buttonup);
+};
 
-$(document).ready(function(){
-    postBlank();
+function buttonDown() {
+    buttondown.classList.add('btnDown');
+    buttondown.textContent = 'Down';
+    li.appendChild(buttondown);
+};
+
+function buttonRemove() {
+    buttonremove.classList.add('btnRemove');
+    buttonremove.textContent = 'Remove';
+    li.appendChild(buttonremove);
+};
+
+//Creates new input pair with up/down/remove triplet appended
+function newInput() {
+    document.querySelector('ul').appendChild(li);
+    inputPair();
+    buttonUp();
+    buttonDown();
+    buttonRemove();
+};
+
+//Calls newInput() when 'Save' button is clicked
+
+document.querySelector('.save').addEventListener('click', () => {
+    newInput();
 });
-//Clicking save:
-    //Changes button to triplet
-        //Check if more than saved pair, generate up/down/delete triplet
-    //generates new blank pairs
-$('ul').click(function(){
-    if ( event.target.className == 'btnSave') {
-        $('.btnSave').parent().remove();
-        $('.inputItem:last-child').append(btnTriplet);
-        postBlank();
-    }
-});
-
-
-//Clicking Up moves selected list entry up:
-$('ul').click(function(){
-    if(event.target.className == 'up') {
-        var upShift = $(event.target).parent().parent();
-        upShift.insertBefore(upShift.prev());
-        //Marks up button disabled if it reaches the top:
-        if(upShift.index() == 0) {
-            $(event.target).attr('disabled','disabled');
-        };
-
-        //Reverses down status:
-        if(event.target.nextElementSibling.getAttribute('disabled') == 'disabled') {
-            $(event.target).next().removeAttr('disabled');
-        };
-    };
-});
-
-//Clicking Down moves selected list entry down:
-$('ul').on('click', function(){
-    if(event.target.className == 'down') {
-        const downShift = $(event.target).parent().parent();
-        downShift.insertAfter(downShift.next());
-        
-        //Reverses up status:
-        if(event.target.previousElementSibling.getAttribute('disabled') == 'disabled') {
-            $(event.target).prev().removeAttr('disabled');
-        };
-
-        //Marks down button disabled if it reaches the bottom: >>>>>><<<<<<
-        $('li:last-child').prev().on('mouseover', function() {
-            if(downShift.next().index() == $('li:last-child').index()) {
-                if($('.down')) {
-                    $(event.target).attr('disabled','disabled');
-                };
-            };
-        });
-    };
-});
-
-//Clicking Delete removes selected list entry:
-
-$('ul').click(function(){
-    if(event.target.className == 'delete') {
-        $(event.target).parent().parent().remove();
-    }
-});
-
-//Right side of page has text entry field
-//Print button at bottom prints entire list/notepad
