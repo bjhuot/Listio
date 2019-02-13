2c597ffda33cb2d8b64ee830c715ab0860923e44
Brian Huot <brian.huot@gmail.com>
2019-02-13 08:01:45 -0500

    up and down fully working

--- js/app.js
+++ js/app.js
@@ -7,12 +7,15 @@
 //
 
 function inputPair(li) {
+    let checkbox = document.createElement('INPUT');
     let input = document.createElement('INPUT');
     let textarea = document.createElement('TEXTAREA');
+    checkbox.setAttribute('type', 'checkbox');
     input.classList.add('itemInput');
     input.setAttribute('placeholder', 'To-Do');
     textarea.classList.add('itemDesc');
     textarea.setAttribute('placeholder', 'Details');
+    li.appendChild(checkbox);
     li.appendChild(input);
     li.appendChild(textarea);
 };
@@ -57,7 +60,29 @@ function copyContent() {
     let destDesc = document.querySelector('ul.saved-items li:last-child .itemDesc');
     destName.value = copyName.value;
     destDesc.value = copyDesc.value;
-}
+};
+
+//Disables Up button at top of list and Down bottom at top of list ((Unless there is only one item))
+function disableCheck() {
+    for(let i=0; i<document.querySelectorAll('.saved-items li').length; i+= 1) {
+        if (i == 0) {
+            document.getElementsByClassName('btnUp')[0].setAttribute('disabled','');
+        } else if (i == document.querySelectorAll('.saved-items li').length-1) {
+            document.getElementsByClassName('btnDown')[i].setAttribute('disabled','');
+        }
+    }
+};
+
+//Enables Up button/Down button once they are moved away from the top/bottom of list
+function enableCheck() {
+    for(let i=0; i<document.querySelectorAll('.saved-items li').length; i+= 1) {
+        if (i == 1 && document.getElementsByClassName('btnUp')[1].disabled == true) {
+            document.getElementsByClassName('btnUp')[1].disabled=false;
+        } else if (i == document.querySelectorAll('.saved-items li').length-2 && document.getElementsByClassName('btnDown')[i].disabled == true) {
+            document.getElementsByClassName('btnDown')[i].disabled=false;
+        }
+    }
+};
 
 //Calls newInput() when 'Save' button is clicked & copies content
 
@@ -66,4 +91,28 @@ document.querySelector('.save').addEventListener('click', () => {
     copyContent();
     document.querySelector('.itemInputPrime').value = '';
     document.querySelector('.itemDescPrime').value = '';
+    disableCheck();
+    enableCheck();
 });
+
+//Moves item up list when Up button is clicked and checks to disable up/down buttons
+document.querySelector('.saved-items').addEventListener('click', () => {
+    if(event.target.classList == 'btnUp') {
+        let liPrev = event.target.parentElement.previousElementSibling;
+        let liCur = event.target.parentElement;
+        let ul = liCur.parentElement;
+        ul.insertBefore(liCur, liPrev);
+    };
+    disableCheck();
+    enableCheck();
+});
+
+//Moves item down list when Down button is clicked and checks to disable up/down buttons
+// document.querySelectorAll('.btnDown').addEventListener('click', () => {
+
+// });
+
+//Removes item from list when Remove button is clicked and checks to disable up/down buttons
+// document.querySelectorAll('.btnRemove').addEventListener('click', () => {
+
+// });
