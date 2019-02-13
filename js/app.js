c2335a457076958163ab8239118d1389523c2f2d
Brian Huot <brian.huot@gmail.com>
2019-02-12 19:51:24 -0500

    fixed saving list items

--- js/app.js
+++ js/app.js
@@ -2,19 +2,13 @@
 // VARIABLES
 //
 
-const li = document.createElement('LI');
-const input = document.createElement('INPUT');
-const buttonup = document.createElement('BUTTON');
-const buttondown = document.createElement('BUTTON');
-const buttonremove = document.createElement('BUTTON');
-const textarea = document.createElement('TEXTAREA');
-
-
 //
 // FUNCTIONS
 //
 
-function inputPair() {
+function inputPair(li) {
+    let input = document.createElement('INPUT');
+    let textarea = document.createElement('TEXTAREA');
     input.classList.add('itemInput');
     input.setAttribute('placeholder', 'To-Do');
     textarea.classList.add('itemDesc');
@@ -23,19 +17,22 @@ function inputPair() {
     li.appendChild(textarea);
 };
 
-function buttonUp() {
+function buttonUp(li) {
+    let buttonup = document.createElement('BUTTON');
     buttonup.classList.add('btnUp');
     buttonup.textContent = 'Up';
     li.appendChild(buttonup);
 };
 
-function buttonDown() {
+function buttonDown(li) {
+    let buttondown = document.createElement('BUTTON');
     buttondown.classList.add('btnDown');
     buttondown.textContent = 'Down';
     li.appendChild(buttondown);
 };
 
-function buttonRemove() {
+function buttonRemove(li) {
+    let buttonremove = document.createElement('BUTTON');
     buttonremove.classList.add('btnRemove');
     buttonremove.textContent = 'Remove';
     li.appendChild(buttonremove);
@@ -43,15 +40,30 @@ function buttonRemove() {
 
 //Creates new input pair with up/down/remove triplet appended
 function newInput() {
-    document.querySelector('ul').appendChild(li);
-    inputPair();
-    buttonUp();
-    buttonDown();
-    buttonRemove();
+    let li = document.createElement('LI');
+    document.querySelector('.saved-items').appendChild(li);
+    inputPair(li);
+    buttonUp(li);
+    buttonDown(li);
+    buttonRemove(li);
 };
 
-//Calls newInput() when 'Save' button is clicked
+//Function to copy content
+
+function copyContent() {
+    let copyName = document.querySelector('.itemInputPrime');
+    let copyDesc = document.querySelector('.itemDescPrime');
+    let destName = document.querySelector('ul.saved-items li:last-child .itemInput');
+    let destDesc = document.querySelector('ul.saved-items li:last-child .itemDesc');
+    destName.value = copyName.value;
+    destDesc.value = copyDesc.value;
+}
+
+//Calls newInput() when 'Save' button is clicked & copies content
 
 document.querySelector('.save').addEventListener('click', () => {
     newInput();
+    copyContent();
+    document.querySelector('.itemInputPrime').value = '';
+    document.querySelector('.itemDescPrime').value = '';
 });
